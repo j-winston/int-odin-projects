9dffce0c35d403c9c21e365504294eb5f271b37a
j-winston <kenjameswinston@gmail.com>
2023-01-09 12:34:50 -0500

    add password validation logic and styling

--- odin-signup-form/script.js
+++ odin-signup-form/script.js
@@ -1,19 +1,19 @@
 //Password validation
 
-const btn = document.querySelector('#create-account');
+const passwordInput = document.querySelector('.form__password-input');
+const passwordConfirmInput = document.querySelector('.form__confirm-input');
 const form = document.querySelector('.form');
 
-let passwd = '';
-let passwd_confirm = '';
 
+passwordConfirmInput.addEventListener('input', () => {
 
-btn.addEventListener('click', (e) => {
-    // prevent form from submitting
-
-    e.preventDefault();
+    let passwd = '';
+    let passwd_confirm = '';
 
     const formData = new FormData(form);
     const values = [...formData.entries()];
+
+    /* Determine if password confirmation matches */ 
 
     for(let i=0; i < values.length; i++ ) {
 
@@ -27,15 +27,57 @@ btn.addEventListener('click', (e) => {
         }
 
     }
-        if (passwd == passwd_confirm) {
-            console.log('match')
-            // display green check mark 
+        if (passwd_confirm && passwd == passwd_confirm) {
+            // to display green check mark if passwords match
+           passwordInput.setAttribute("match", true);
+           passwordConfirmInput.setAttribute("match", true);
+
         } else {
             if (passwd != passwd_confirm) {
-                console.log('passwords do not match');
-                // display warning message 
+                // Will now display warning message and red outline
+                passwordInput.setAttribute('match', false);
+                passwordConfirmInput.setAttribute('match', false);
+                
             }
         } 
+ /* Check if password matches confirmation */ 
+    passwordInput.addEventListener('input', () => {
 
-})
+        let passwd = '';
+        let passwd_confirm = '';
+
+        const formData = new FormData(form);
+        const values = [...formData.entries()];
+
+        for(let i=0; i < values.length; i++ ) {
+
+            if (values[i][0] == 'user_password') {
+                passwd = values[i][1];
+
+            }
+
+            if (values[i][0] == 'user_password_confirm') {
+                passwd_confirm = values[i][1];
+            }
+
+        }
+            if (passwd == passwd_confirm) {
+                // to display green check mark if passwords match
+               passwordInput.setAttribute("match", true);
+               passwordConfirmInput.setAttribute("match", true);
+
+            } else {
+                if (passwd != passwd_confirm) {
+                    // Will now display warning message and red outline
+                    passwordInput.setAttribute('match', false);
+                    passwordConfirmInput.setAttribute('match', false);
+                    
+                }
+            } 
+        
+
+    });
+
+});
+
 
